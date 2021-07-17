bb8b41d253e5619f7ef80fccb8cc04c09cd22e33
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-07-17 18:07:03 +0900

    記述を変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = [
-      "今日はRailsの学習です。",
-      "プログラミング、意外と簡単。",
-      "この調子で続けて行こう。",
-    ]
+    @posts = Post.all
   end
 end
