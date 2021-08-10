994211baf85b5588ee2bdfe0c04aa263274df68e
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-08-10 21:18:29 +0900

    取得した画像データをファイルに書き込み

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
-    @user.image = "user_#{@user.id}.png"
+    if params[:image]
+      @user.image = "user_#{@user.id}.png"
+      File.binwrite("public/user_images/#{@user.image}", params[:image].read)
+    end
     if @user.save
       flash[:notice] = 'ユーザー情報を編集しました。'
       redirect_to user_path @user
