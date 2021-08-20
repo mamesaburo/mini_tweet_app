f74b1eadb41d663d7990e3214e81a9079cc006cf
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-08-20 21:48:04 +0900

    ログインユーザーをbefore_actionを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_form_forgery with: :exception
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find(session[:user_id]) if session[:user_id]
+  end
 end
