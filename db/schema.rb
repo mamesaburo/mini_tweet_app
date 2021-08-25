fb2547aecb3e8f7eb70e609d33f232ad121e7741
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-08-25 20:58:03 +0900

    postsテーブルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_141631) do
+ActiveRecord::Schema.define(version: 2021_08_25_115604) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
